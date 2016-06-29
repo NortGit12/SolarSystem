6a5fe176630ade3ebf1a066cfc0191aa41ead540
Jeff Norton <jeff12ntn@gmail.com>
2016-06-28 18:09:28 -0600

    Committing staged files

--- SolarSystem/PlanetDetailViewController.swift
+++ SolarSystem/PlanetDetailViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class PlanetDetailViewController: UIViewController {
 
+    @IBOutlet weak var diameterLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lengthOfDayLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var planet: Planet?
+    
+    func updateWithPlanet(planet: Planet) {
+        self.title = planet.name
+        distanceLabel.text = "Kilometers from the sun: \(planet.millionKMsFromSun)"
+        diameterLabel.text = "Diameter: \(planet.diameter)"
+        lengthOfDayLabel.text = "Length of day: \(planet.dayLength)"
+        imageView.image = UIImage(named: planet.name.lowercaseString)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let planet = planet{
+            updateWithPlanet(planet)
+        }
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
